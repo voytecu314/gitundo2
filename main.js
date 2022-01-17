a76ffb4ce36a53bc118e4178096cba1ed09e58b0
WojtekU <voytecu@gmail.com>
2022-01-17 13:15:17 +0100

    code shortend(msg function created)

--- main.js
+++ main.js
@@ -15,6 +15,20 @@ const creatToDo = () => {
         }
     }
 
+
+
+//Function to find objects in toDoList array
+//uses getToDoList to have list of todos
+//uses index number that prompted by user
+//calls switchDone function to switch value of toDoItem.isDone
+
+const getItemToSwitchDone = () => {
+    let msg = prepMsg('switch status');
+    let itemIndex=checkIndex(msg);
+    if(itemIndex===null) return 0;
+    else switchDone(toDoList[itemIndex]);//??
+}
+
 //Function to read from array and prepare a list to show on prompt msg
 
 const getToDoList = () => {
@@ -27,45 +41,16 @@ const getToDoList = () => {
     return list;
 }
 
-//Function to find objects in toDoList array
-//uses getToDoList to have list of todos
-//uses index number that prompted by user
-//calls switchDone function to switch value of toDoItem.isDone
-
-const getItemToSwitchDone = () => {
-    let msg =`
-    ${getToDoList()}
-    Please type a number of the item to switch status
-    `;
-    let itemIndex=checkIndex(msg);
-    if(itemIndex===null) return 0;
-    else switchDone(toDoList[itemIndex]);//??
-}
-
-
 //Func. to switch isDone property value
 const switchDone = (item) => {
 item.isDone = !item.isDone;
 // !item.isDone would work on its own
 }
 
-//check if inputed index is valid
-
-const checkIndex = (display) => {
-    let itemIndex;
-    do{
-        itemIndex = prompt(display,`Type integer numbers from 0 to ${toDoList.length - 1}`);
-        if(itemIndex===null) break;
-    }while(!Number.isInteger(parseFloat(itemIndex)) || itemIndex < 0 || itemIndex > toDoList.length-1 || isNaN(parseInt(itemIndex)))
-    return itemIndex;
-}
 
 //Edit item function
 const editItem = () => {
-    let msg =`
-    ${getToDoList()}
-    Please type a number of the item to edit
-    `;
+    let msg = prepMsg('edit');
     let itemIndex = checkIndex(msg);
     if(itemIndex===null) return 0;
     let newText = prompt('Please update the task',toDoList[itemIndex].task);
@@ -83,13 +68,31 @@ const editItem = () => {
 //delete todo from array
 
 const deleteItem = () => {
-    let msg =`
-    ${getToDoList()}
-    Please type a number of the item to delete
-    `;
+    let msg = prepMsg('delete');
     let itemIndex=checkIndex(msg);
     if(itemIndex===null) return 0;
     else toDoList.splice(itemIndex,1);
+}
+
+//check if inputed index is valid
+
+const checkIndex = (display) => {
+    let itemIndex;
+    do{
+        itemIndex = prompt(display,`Type integer numbers from 0 to ${toDoList.length - 1}`);
+        if(itemIndex===null) break;
+    }while(!Number.isInteger(parseFloat(itemIndex)) || itemIndex < 0 || itemIndex > toDoList.length-1 || isNaN(parseInt(itemIndex)))
+    return itemIndex;
+}
+
+//Prepare correct prompt message
+
+const prepMsg = (text) => {
+    let msg =`
+    ${getToDoList()}
+    Please type a number of the item to ${text}
+    `;
+    return msg;
 }
 
 
@@ -97,16 +100,16 @@ const deleteItem = () => {
 
 let toDoItem;
 do {
-    //Menu on prompt:
+//Menu on prompt:
 
-const menu =`MENU:
-0. Create new To-Do task
-1. Make any item done or undone
-2. Edit any item on the list
-3. Delete any item
-4. Show the list
-q. Quit.
-`;
+    const menu =`MENU:
+    0. Create new To-Do task
+    1. Make any item done or undone
+    2. Edit any item on the list
+    3. Delete any item
+    4. Show the list
+    q. Quit.
+    `;
     toDoItem = prompt(menu);
 
     switch(toDoItem){
